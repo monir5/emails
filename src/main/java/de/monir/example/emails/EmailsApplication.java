5d94013ba9ab96acd4d297a6a25559393cc5dc5a
monir <monir.ahammod@brockhaus-ag.de>
2024-08-06 22:38:16 +0200

    feat(doc): update springdoc swagger

--- src/main/java/de/monir/example/emails/EmailsApplication.java
+++ src/main/java/de/monir/example/emails/EmailsApplication.java
@@ -1,9 +1,25 @@
 package de.monir.example.emails;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Email Service",
+				version = "1.0.0",
+				description = "This Email Service exposes a REST API to execute CRUD operations. Endpoints are " +
+						"under the route \"/emails\" and the information corresponding to the email contents are " +
+						"being stored in a database.",
+				contact = @Contact(
+						name = "Monir M. Ahammod",
+						email = "monir.ahammod@gmail.com"
+				)
+		)
+)
 public class EmailsApplication {
 
 	public static void main(String[] args) {
