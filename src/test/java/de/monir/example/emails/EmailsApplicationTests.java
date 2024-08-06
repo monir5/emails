56a3ccaba4524fad4754ca5f35a5fa14475ab306
monir <monir.ahammod@brockhaus-ag.de>
2024-08-06 14:37:15 +0200

    Initial empty project of emails

---
+++ src/test/java/de/monir/example/emails/EmailsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.monir.example.emails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
