4eeb6eec56db353b2d9fa9ec869bddb6fba0d983
monir <monir.ahammod@brockhaus-ag.de>
2024-08-06 18:29:15 +0200

    feat(database): remove h2, add postgresql, flyway and springdoc

---
+++ src/main/resources/db/migration/V1__create_emails_table_and_dependents.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE emails (
+    "id" BIGINT PRIMARY KEY,
+    "from" VARCHAR(100) NOT NULL,
+    "subject" VARCHAR(500),
+    "body" VARCHAR(3000),
+    "state" SMALLINT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE email_address_to (
+    "email_id" BIGINT,
+    "email" VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE email_address_cc (
+    "email_id" BIGINT,
+    "email" VARCHAR(50) NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS email_id_sequence as BIGINT INCREMENT BY 1 START 1 OWNED BY emails.id;
